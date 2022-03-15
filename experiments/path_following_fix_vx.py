30eedf6a24697cc8a795ba8f04b8deab50a8f472
Kohei Kumazaki <kumazaki98@gmail.com>
2022-03-16 04:41:05 +0900

    fix

--- experiments/path_following_fix_vx.py
+++ experiments/path_following_fix_vx.py
@@ -22,7 +22,6 @@ def sim(simT, dt, plant, controller):
     yp = plant.observe()
 
     u = adaptive_ctrl.calc_u(yp, r)
-    u = np.clip(u, -1.0, 1.0)
     xp = plant.x
     plant.update(dt, [-xp[5]*xp[4]*plant.vehicle_param.mass, u])
     #plant.update(dt, u)
@@ -36,17 +35,38 @@ def sim(simT, dt, plant, controller):
     yield t, yp[0], yr[0], e, theta, u[0], r[0], plant.x
 
 
+def data_header(plant_param, reference_param):
+  return f'''
+    Plant Parameters 
+      type : {plant_type}
+    {plant_param}
+     
+    MRAC parameter
+     gain : {adaptive_gain}
+     lbd0 : {lbd0}
+     vx   : {vx}
+    Reference Parameters
+    {reference_param}
+
+    use initial guess : {use_initial_guess}
+
+    analytical adaptive param:
+    {estTheta} 
+  '''
+
+
 road = np.loadtxt("../data/path.txt", delimiter=" ", dtype=np.float32)
 tree = KDTree(road[:, :2])
 
 plantParam = VehicleParam()
+'''
 plantParam.mass = 1727.0
 plantParam.cf = 94000.0
 plantParam.cr = 94000.0
 plantParam.lf = 1.17
 plantParam.lr = 1.42
 plantParam.Iz = 2867.0
-'''
+
 plantParam.mass = 5500.0
 plantParam.cf = 50000.0
 plantParam.cr = 130000.0
@@ -55,15 +75,44 @@ plantParam.lr = 1.5
 plantParam.Iz = 20600.0
 '''
 
+
+## Parameter settings
 vx = 10.0
 lbd0 = [1, 1]
+plant_type = "truck"
+adaptive_gain = 100.0
+umax = 0.1
+umin = -0.1
+
+filename_prefix = plant_type + "_vx_10_gain_100"
+use_initial_guess = True
+
+## processing
+if plant_type == "vehicle":
+  plantParam.mass = 1727.0
+  plantParam.cf = 94000.0
+  plantParam.cr = 94000.0
+  plantParam.lf = 1.17
+  plantParam.lr = 1.42
+  plantParam.Iz = 2867.0
+
+elif plant_type == "truck":
+  plantParam.mass = 5500.0
+  plantParam.cf = 50000.0
+  plantParam.cr = 130000.0
+  plantParam.lf = 2.5
+  plantParam.lr = 1.5
+  plantParam.Iz = 20600.0
+else:
+  plant_type = "prius"
+
 
 ## Initial parameter
 plantInit = [road[0,0], road[0,1], road[0,2], vx, 0, 0, 0]
 
 ## Reference model
 modelParam = VehicleParam()
-A, B, C, D = linear_vehicle_model_fb(modelParam, 20.0, -2.0, -0.05)
+A, B, C, D = linear_vehicle_model_fb(modelParam, vx, -2.0, -0.05)
 mss = ss(A, B, C, D)
 ref = LinearSystem(mss, 0, "ref")
 
@@ -71,13 +120,12 @@ sv_dim, L, l = designed_state_space_eq(mss, lbd0)
 w1 = LinearSystem(ss(L, l, np.identity(sv_dim), 0), 0, "w1")
 w2 = LinearSystem(ss(L, l, np.identity(sv_dim), 0), 0, "w2")
 
-
 phi_dim = sv_dim*2 + ref.sys.ninputs + ref.sys.noutputs
 Lp = -np.identity(phi_dim)*1
 lp = np.identity(phi_dim)*1
 phi = LinearSystem(ss(Lp, lp, np.identity(phi_dim), 0), 0, 'phi')
 
-adaptive_ctrl = AdaptiveControllerN2(ref, w1, w2, phi, 10.0)
+adaptive_ctrl = AdaptiveControllerN2(ref, w1, w2, phi, adaptive_gain, umin=umin, umax=umax)
 
 # set estimated param
 
@@ -88,7 +136,7 @@ pss = ss(A, B, C, D)
 print(tf(pss))
 print(tf(mss))
 
-estTheta = estimate_theta(pss, mss, 3, [1,1])
+estTheta = estimate_theta(pss, mss, sv_dim, lbd0)
 print(estTheta)
 adaptive_ctrl.theta[-2] = -0.05
 
@@ -126,11 +174,17 @@ while (1):
     break
 
 
-np.savetxt("../data/output/no_init_param/adaptive_vehicle_vx_10_gain_10_theta.csv", np.array(thetas), delimiter=" ")
-np.savetxt("../data/output/no_init_param/adaptive_vehicle_vx_10_gain_10_u.csv", np.array(us), delimiter=" ")
-np.savetxt("../data/output/no_init_param/adaptive_vehicle_vx_10_gain_10.csv", np.array(pxs), delimiter=" ")
-np.savetxt("../data/output/no_init_param/adaptive_vehicle_vx_10_gain_10_yp.csv", yps, delimiter=" ")
-np.savetxt("../data/output/no_init_param/adaptive_vehicle_vx_10_gain_10_e.csv", es, delimiter=" ")
+header = data_header(plantParam, modelParam)
+
+foldername = "with_init_param" if use_initial_guess else "no_init_param"
+
+print("## results")
+print("../data/output/" + foldername +"/" + filename_prefix + ".csv")
+np.savetxt("../data/output/" + foldername +"/" + filename_prefix + ".csv", np.array(pxs), delimiter=" ", header=header)
+np.savetxt("../data/output/" + foldername +"/" + filename_prefix + "_theta.csv", np.array(thetas), delimiter=" ", header=header)
+np.savetxt("../data/output/" + foldername +"/" + filename_prefix + "_u.csv", np.array(us), delimiter=" ", header=header)
+np.savetxt("../data/output/" + foldername +"/" + filename_prefix + "_yp.csv", yps, delimiter=" ", header=header)
+np.savetxt("../data/output/" + foldername +"/" + filename_prefix + "_e.csv", es, delimiter=" ", header=header)
 #np.savetxt("../data/output/no_init_param/adaptive_track.csv", np.array(res["plant"]), delimiter=" ")
 #np.savetxt("../data/output/no_init_param/adaptive_track_yp.csv", np.array(res["yp"]), delimiter=" ")
 #np.savetxt("../data/output/no_init_param/adaptive_track_theta.csv", np.array(res["theta"]), delimiter=" ")
