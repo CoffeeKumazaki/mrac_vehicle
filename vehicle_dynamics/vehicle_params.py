9ed68c467c5bb30c0d61e588d1901f31d4ef5399
Kohei Kumazaki <kumazaki98@gmail.com>
2022-03-16 05:12:33 +0900

    add repr

--- vehicle_dynamics/vehicle_params.py
+++ vehicle_dynamics/vehicle_params.py
@@ -9,3 +9,18 @@ class VehicleParam():
     self.lr = 1.6
     self.zeta = 0.7
     self.omega_n = 17.5
+
+  def __repr__(self) -> str:
+    return f'''Vehicle Parameter
+      mass:    {self.mass}
+      Iz:      {self.Iz}
+      cf:      {self.cf}
+      cr:      {self.cr}
+      lf:      {self.lf}
+      lr:      {self.lr}
+      zeta:    {self.zeta}
+      omega_n: {self.omega_n}
+    '''
+
+  def __str__(self) -> str:
+      return self.__repr__()
